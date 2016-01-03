603a2726742a05ed5553969fb3fec06674165508
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-03 00:12:26 +0000

    Done.

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+
+def sum_square_difference(max)
+  naturals = (1..max).to_a
+  squares = [1]
+  
+  sum = naturals.inject do |s, n|
+    squares << n * n
+    s + n
+  end
+  
+  square_of_sum  = sum * sum
+  sum_of_squares = squares.inject { |s, n| s + n }
+  difference = square_of_sum - sum_of_squares
+  
+  difference
+end
